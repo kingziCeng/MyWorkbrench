f6725d4d0c9e2eb9478e7aa6abe8ff880ac79bd5
kingzi <kingzizeng@gmail.com>
2023-02-04 20:59:06 +0800

    工作台设计，第一版

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import { Button } from 'antd'
-import { useState } from 'react'
-
+import { Layout, Menu } from 'antd';
+import { ContainerOutlined, ApiFilled } from '@ant-design/icons'
 function App() {
+  const { Header, Content, Sider } = Layout;
   return (
-    <div>
-      <Button type='primary'>test</Button>
-    </div>
+    <Layout style={{height: '100vh', width: '100vw'}}>
+      <Header style={{ display: 'flex' }}>
+        <div style={{ color: 'white', width: '150px', 'font-size': '20px' }}>
+          工作台
+        </div>        
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['workbrench']}
+          items={[
+            {
+              key: 'workbrench',
+              label: '我的工作台'
+            }
+          ]}
+        />
+      </Header>
+      <Layout>
+        <Sider
+            width={200}
+        >
+         <Menu
+          mode="inline"
+          style={{
+            height: '100%',
+            borderRight: 0,
+          }}
+          defaultSelectedKeys={['planeDate']}
+          items={[
+            {
+              key: "planeDate",
+              label: '计划日历',
+              icon: <ContainerOutlined />
+            },
+            {
+              key: "planeLine",
+              label: '计划时间线',
+              icon: <ApiFilled />
+            }, 
+          ]}
+        />
+        </Sider>
+        <Layout>
+          <Content>123123</Content>
+        </Layout>
+      </Layout> 
+    </Layout>
   )
 }
 
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+export const firstLevelRouter = [
+  {
+    key: 'workbrench',
+    label: '我的工作台'
+  },
+  {
+    key: 'mynote',
+    label: '我的笔记'
+  }
+]
+
+export const secondeLevelRouter = [
+  {
+    key: "myplane",
+    label: '我的计划'
+  }
+]
